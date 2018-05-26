0601296c4e26f97fc90fd5336aa98bb134e05e66
Jerry U <jerry@lyshnia.com>
2018-05-27 02:25:15 +0800

    updated objects in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include "SE1/Bart.h"
 #include "SE1/Lisa.h"
 
@@ -7,16 +8,16 @@ using namespace std;
 
 int main() {
 
-    Bart *bart = new Bart("Bart J. Simpson");
-    Lisa *lisa = new Lisa("Lisa J. Simpson");
+    Bart bart = Bart("Bart J. Simpson");
+    Lisa lisa = Lisa("Lisa J. Simpson");
 
     cout << "Burns World! v0.3.1" << endl << endl;
 
-    cout << "Name: " << bart->getName() << endl;
-    cout << "Hairstyle: " << bart->HairStyle() << endl;
+    cout << "Name: " << bart.getName() << endl;
+    cout << "Hairstyle: " << bart.HairStyle() << endl;
     cout << "~~~~~~~~~~~~~~~~~~~~~\n";
-    cout << "Name: " << lisa->getName() << endl;
-    cout << "Hairstyle: " << lisa->HairStyle() << endl;
+    cout << "Name: " << lisa.getName() << endl;
+    cout << "Hairstyle: " << lisa.HairStyle() << endl;
 
     return 0;
 }
